19a63b65d7cd6cc69e41044e51f02fecc0950b10
mike mambwe <mikeapple056@gmail.com>
2023-03-10 20:38:15 +0200

    main js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import './App.css';
+
+function App() {
+  const bg = "https://pixabay.com/images/id-3608029/"
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
