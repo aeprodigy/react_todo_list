8d9ae8a69aaba8cf088929ae098de8dc0e25076f
mike mambwe <mikeapple056@gmail.com>
2023-03-15 21:55:14 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-import './App.css';
-
-function App() {
-  const bg = "https://pixabay.com/images/id-3608029/"
+/*
+author: MIKE MAMBWE
+Program: a react todo list APP
+Contact: +260975854067
+Email : engineermambwe@gmail.com
+*/
+//this program uses react hooks to store and output the information in it.
+import React from 'react'
+import './App.css'
+const App = () => {
   return (
-    <div className="App">
-     
+    <div className='App'>
+      <h1>Hello</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
