61db3610618bdac08ea9af23918fc628cb32d78a
mike mambwe <mikeapple056@gmail.com>
2023-03-17 12:41:14 +0200

    second bug fix

--- src/App.js
+++ src/App.js
@@ -5,14 +5,55 @@ Contact: +260975854067
 Email : engineermambwe@gmail.com
 */
 //this program uses react hooks to store and output the information in it.
-import React from 'react'
-import './App.css'
-const App = () => {
-  return (
-    <div className='App'>
-      <h1>Hello</h1>
-    </div>
-  )
-}
+import React, { useState } from "react";
 
-export default App
+import "./App.css";
+import Footer from "./Components/Footer";
+const App = () => {
+  //The use state hook below this line
+  const [todo, setTodo] = useState([]);
+  //This todo sets  track of the currect todo we are adding.
+  const [trackTodo, setTrackTodo] = useState("");
+
+  //The handleSubmit Method
+  function handleSubmit(e) {
+    e.preventDefault(); // this statement prevents the reload of the page when the form is submitted,
+
+    const todoObj = {
+      id: new Date().getTime(),
+      text: trackTodo,
+      completed: false,
+    };
+    setTodo([...todo].concat(todoObj)); //fixed bug.variable
+    setTrackTodo("");
+  }
+
+  //The delete function to remove the todo from the list
+  function deleteTodo(){
+
+  }
+
+  return (
+    <div className="App">
+      <h2>First week(1) Todo List App</h2>
+      {/*The Todo List form starts here */}
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTrackTodo(e.target.value)}
+          value={trackTodo}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {todo.map((trackTodo) => (
+        <div key={trackTodo.id}>
+          <div>{trackTodo.text}</div>
+          <button onClick={()=> deleteTodo(trackTodo.id)}>Remove</button>
+        </div>
+      ))}
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
